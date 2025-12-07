b337752270d08d7d0c98fd4701cf3827610063d9
AlexxIacob <alexandruiacob01@gmail.com>
2025-12-07 15:43:25 +0200

    fix database config

---
+++ CompanieZbor/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username, password, admin)
+VALUES ('test', 'test123', true);
+
+INSERT INTO users (username, password, admin)
+VALUES ('test1', 'test123', true);
+
+
+INSERT INTO flight (departure, arrival, departure_time, duration, nr_seats, plane_name, price)
+VALUES ('Bucuresti', 'Paris', '2025-01-15 10:30:00', 180, 180, 'Boeing 737', 199.99);
